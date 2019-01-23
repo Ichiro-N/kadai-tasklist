eaa345d51fd7b29d1103bf4759463a93ec51f38e
Ichiro <study.Ichiro@gmail.com>
2019-01-23 13:31:47 +0000

    correct mistakes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in, only: [:show, :new, :create, :edit, :update, :destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:destroy]
 
   def index
     if logged_in?
@@ -50,7 +52,7 @@ class TasksController < ApplicationController
   private
   
   def set_task
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find_by(id: params[:id])
   end
 
   # Strong Parameter
